c085f949703f131e17a01c1bafeab17ab4134231
zodunn <zodunn@uwm.edu>
2024-10-17 19:48:34 -0500

    Recenter

--- Scripts/XRSetup.gd
+++ Scripts/XRSetup.gd
@@ -12,5 +12,15 @@ func _ready():
 
 		# Change our main viewport to output to the HMD
 		get_viewport().use_xr = true
+		
+		# Connect the OpenXR events
+		xr_interface.pose_recentered.connect(_on_openxr_pose_recentered)
 	else:
 		print("OpenXR not initialized, please check if your headset is connected")
+
+
+# Handle OpenXR pose recentered signal
+func _on_openxr_pose_recentered() -> void:
+	# User recentered view, we have to react to this by recentering the view.
+	# This is game implementation dependent.
+	XRServer.center_on_hmd(XRServer.RESET_BUT_KEEP_TILT, true)
